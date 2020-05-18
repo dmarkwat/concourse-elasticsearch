726a462eaaba85894e644682d26c5f1312baffd3
Dan Markwat <daniel.markwat@gmail.com>
2020-05-18 08:45:44 -0400

    Updates tests for creating indices

--- pkg/es/client_test.go
+++ pkg/es/client_test.go
@@ -2,12 +2,14 @@ package es
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	elastic "github.com/elastic/go-elasticsearch/v7"
 	"github.com/elastic/go-elasticsearch/v7/esapi"
 	"github.com/google/uuid"
+	"strconv"
 	"strings"
 	"testing"
 	"time"
@@ -27,8 +29,12 @@ func NewTestClient() *elastic.Client {
 	return es
 }
 
+func NewIndexName(prefix string) string {
+	return fmt.Sprintf("%s-%s", prefix, uuid.New().String())
+}
+
 func NewIndex(es *elastic.Client, indexPrefix string, settings map[string]interface{}) (string, error) {
-	index := fmt.Sprintf("%s-%s", indexPrefix, uuid.New().String())
+	index := NewIndexName(indexPrefix)
 	var request []func(*esapi.IndicesCreateRequest)
 	if settings != nil {
 		body, err := json.Marshal(settings)
@@ -336,6 +342,84 @@ func TestLatestBySortFields(t *testing.T) {
 		if len(docs) != 0 {
 			t.Errorf("No documents should be returned; got %d", len(docs))
 			return
+		}
+	})
+}
+
+func TestCreateIndex(t *testing.T) {
+	fieldMap := map[string]PropertyMapping{
+		"timestamp": {
+			Type: "date",
+		},
+	}
+	var sortFields []string
+	for field, _ := range fieldMap {
+		sortFields = append(sortFields, field)
+	}
+	es := NewTestClient()
+	t.Run("Working", func(t *testing.T) {
+		name := NewIndexName("testcreateindexworking")
+		err := CreateIndex(es, name, fieldMap, sortFields)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Cleanup(CleanupIndex(t, es, name))
+
+		ordered := []string{
+			`{"timestamp":null}`,
+			`{"timestamp":"2020-01-01T00:00:00Z"}`,
+			`{"timestamp":"2021-01-01T00:00:00Z"}`,
+		}
+		for idx, item := range ordered {
+			create, err := es.Create(name, strconv.Itoa(idx), bytes.NewReader([]byte(item)))
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if create.IsError() {
+				t.Errorf("Error creating test document, %d", idx)
+				return
+			}
+		}
+
+		err = RefreshIndex(es, name)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		search, err := es.Search(
+			es.Search.WithContext(context.Background()),
+			es.Search.WithIndex(name),
+		)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		if search.IsError() {
+			t.Errorf("Error searching: %s", search.Status())
+			return
+		}
+
+		defer search.Body.Close()
+
+		var envelope EnvelopeResponse
+		err = json.NewDecoder(search.Body).Decode(&envelope)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		for idx, value := range envelope.Hits.Hits {
+			idInt, err := strconv.Atoi(value.ID)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			if idx != idInt {
+				t.Errorf("Document ID, %s, returned out of order", value.ID)
+			}
 		}
 	})
 }
